3334a1fcc01f9bb2c535c827b74d042cbdfa14e1
karenvicent <karenvergara@msn.com>
2022-06-24 03:22:12 +0000

    parte2

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Characters, Planets, Favorite_characters, Favorite_planets
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,28 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/characters', methods=['GET'])
+def get_all_characters():
+    all_charaters_query = Characters.query.all()
+    all_characters = list(map(lambda x: x.serialize(), all_charaters_query))
+    return jsonify(all_characters), 200
+
+@api.route('/planets', methods=['GET'])
+def get_all_planets():
+    all_planets_query = Planets.query.all()
+    all_planets = list(map(lambda x: x.serialize(), all_planets_query))
+    return jsonify(all_planets), 200
+
+@api.route('/addfavoriteplanet/<int:id>/', methods=['POST'])
+def add_planet(id):
+    planet_query = Planets.query.filter_by(id)
+    favorite_planet = Favorite_planets(planet_name=planet_query['planet_name'])
+    db.session.add(favorite_planet)
+    db.session.commit()
+
+    respuesta = {
+    "message": "favorito agregado exitosamente"
+    }
+
+    return jsonify(respuesta), 200
