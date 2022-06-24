2078cd57351406f0baee1918515eb4b5ec60a933
karenvicent <karenvergara@msn.com>
2022-06-24 02:00:34 +0000

    parte 1

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,67 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(200), unique=True, nullable=False)
+    gender = db.Column(db.String(100))
+    url = db.Column(db.String(500), unique=True)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.character_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character_name": self.character_name,
+            "gender": self.gender,
+            "url": self.url,
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(200), unique=True, nullable=False)
+    orbit = db.Column(db.String(200))
+    url = db.Column(db.String(500), unique=True)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.planet_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_name": self.planet_name,
+            "orbit": self.orbit,
+            "url": self.url,
+            # do not serialize the password, its a security breach
+        }
+
+class Favorite_characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(200), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return '<Favorite_characters %r>' % self.character_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "character_name": self.character_name,
+            # do not serialize the password, its a security breach
+        }
+
+class Favorite_planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(200), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return '<Favorite_planets %r>' % self.planet_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_name": self.planet_name,
+            # do not serialize the password, its a security breach
+        }
